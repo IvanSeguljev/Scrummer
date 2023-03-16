b06897988eb63a8b21b5a7f5ff908c001612e675
Ivan <ivan.seguljev@yahoo.com>
2023-03-16 15:52:23 +0100

    init

---
+++ scrummer-api/src/main/java/com/iseguljev/scrummerapi/ScrummerApiApplication.java
@@ -0,0 +1,13 @@
+package com.iseguljev.scrummerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrummerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrummerApiApplication.class, args);
+	}
+
+}
---
+++ scrummer-api/src/test/java/com/iseguljev/scrummerapi/ScrummerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iseguljev.scrummerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrummerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
